3f01349d76f92e8f92048b9e171c113bcba0e5f9
Luis Sanchez <sanchezl391@gmail.com>
2018-10-29 16:12:46 -0600

    changed to ES6 format

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLock, faTimesCircle, faCaretUp, faCaretDown, faCaretRight, faPause, faPlay, faCaretLeft,faCommentAlt, faClock, faUserAlt } from '@fortawesome/free-solid-svg-icons'
 
-// import registerServiceWorker from './registerServiceWorker';
-const registerServiceWorker = require('./registerServiceWorker');
-
+import registerServiceWorker from './registerServiceWorker';
 
 library.add(faCaretUp, faLock, faUserAlt, faTimesCircle, faCaretDown, faCaretLeft, faPlay, faPause, faCaretRight, faClock, faCommentAlt);
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,27 +1,27 @@
-module.exports = {
-    registerServiceWorker: async function() {
-        console.log('registering service worker');
-        // registering service worker
-        const register = await navigator.serviceWorker.register('./worker.js', {
-          scope: '/' // url for where the service worker should work
-        });
-    
-        // service worker registered
-        // register push
-        console.log('registering push');
-        const subscription = await register.pushManager.subscribe({
-          userVisibleOnly:true,
-          applicationServerKey: this.urlBase64ToUint8Array(process.env.publicVapidKey)
-        });
-    
-        console.log('sending push notification');
-        // send push notification
-        await fetch('https://task-focus.herokuapp.com/subscribe', {
-          method: 'POST',
-          body: JSON.stringify(subscription),
-          headers: {
-            'Content-type': 'application/json; charset=utf-8'
-          }
-        });
-    }
+async function registerServiceWorker() {
+    console.log('registering service worker');
+    // registering service worker
+    const register = await navigator.serviceWorker.register('./worker.js', {
+        scope: '/' // url for where the service worker should work
+    });
+
+    // service worker registered
+    // register push
+    console.log('registering push');
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly:true,
+        applicationServerKey: this.urlBase64ToUint8Array(process.env.publicVapidKey)
+    });
+
+    console.log('sending push notification');
+    // send push notification
+    await fetch('https://task-focus.herokuapp.com/subscribe', {
+        method: 'POST',
+        body: JSON.stringify(subscription),
+        headers: {
+        'Content-type': 'application/json; charset=utf-8'
+        }
+    });
 }
+
+export default registerServiceWorker;
