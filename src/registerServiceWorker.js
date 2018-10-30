22679b5cb86de25c8bd11a27c42c40e953e59db2
Luis Sanchez <sanchezl391@gmail.com>
2018-10-29 19:56:14 -0600

    logging server key

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -64,6 +64,7 @@ const isLocalhost = Boolean(
     for (let i = 0; i < rawData.length; ++i) {
       outputArray[i] = rawData.charCodeAt(i);
     }
+    console.log('app server key: ' + outputArray);
     return outputArray;
   }
 
@@ -77,6 +78,7 @@ const isLocalhost = Boolean(
           applicationServerKey: urlBase64ToUint8Array('BNg3yom88DUki2fR0vfO_JQX9amUnGrjAYizv3OvH4Mc5vK8TVSE2zEeFRgGLAj3F_ZvwQjejlNf_X2nbMqhTgE')
         });
         console.log('sending push notification');
+        console.log('subscription JSON: ' + JSON.stringify(subscription));
         // send push notification
         fetch('https://task-focus-api.herokuapp.com/subscribe', {
             method: 'POST',
